d4997cc908c4c184b035e8c5c99f27337d292669
KLZ-0 <adrian@kalazi.com>
2021-04-24 00:01:30 +0200

    Initialize pcap

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <pcap/pcap.h>
 #include "config.h"
 #include "sniffer.h"
 
 int main(int argc, char *argv[]) {
+	char errbuf[PCAP_ERRBUF_SIZE];
+	if (pcap_init(0, errbuf)) {
+		std::cerr << "PCAP initialization error: " << errbuf << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	Sniffer sniffer = Sniffer(argc, argv);
 
 	try {
