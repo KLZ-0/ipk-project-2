f81b925efc50ee12ee50e2b3f4a189bcfaf35e27
KLZ-0 <adrian@kalazi.com>
2021-04-24 15:17:21 +0200

    Add some other shit to packet callback

--- src/sniffer.cpp
+++ src/sniffer.cpp
@@ -3,13 +3,13 @@
 #include <net/if.h>
 #include <pcap.h>
 #include <functional>
+#include <net/ethernet.h>
 #include "sniffer.h"
 
-static Config *glob_config;
+#include <netinet/ip_icmp.h>
 
 Sniffer::Sniffer(int argc, char **argv) {
 	config = new Config(argc, argv);
-	glob_config = config;
 	if (config->only_interfaces) {
 		print_interfaces();
 		delete config;
@@ -56,16 +56,49 @@ void Sniffer::run() {
 
 	int pa = pcap_activate(pcap);
 	if (pa < 0) {
+		pcap_close(pcap);
 		throw std::runtime_error(pcap_geterr(pcap));
 	} else if (pa > 0) {
 		std::cerr << "PCAP warning: " << pcap_geterr(pcap) << std::endl;
 	}
 
-	pcap_loop(pcap, config->num, packet_callback, nullptr);
+	if (pcap_set_datalink(pcap, DLT_EN10MB)) {
+		pcap_close(pcap);
+		throw std::runtime_error(pcap_geterr(pcap));
+	}
+
+	if (pcap_loop(pcap, config->num, packet_callback, (u_char*) this)) {
+		pcap_close(pcap);
+		throw std::runtime_error(pcap_geterr(pcap));
+	}
 
 	pcap_close(pcap);
 }
 
-void Sniffer::packet_callback(u_char *user, const struct pcap_pkthdr *header, const u_char *bytes) {
-	std::cout << "Packet size: " << header->caplen << "/" << header->len << std::endl;
+void Sniffer::packet_callback(u_char *user, const struct pcap_pkthdr *header, const u_char *payload) {
+	auto *sniffer = (Sniffer*) user;
+
+	std::cout << "Packet (";
+
+	auto *eth_header = (struct ether_header *) payload;
+	uint16_t packet_type = ntohs(eth_header->ether_type);
+
+	switch (packet_type) {
+		case ETHERTYPE_IP:
+			std::cout << "IP";
+			break;
+		case ETHERTYPE_IPV6:
+			std::cout << "IPv6";
+			break;
+		case ETHERTYPE_ARP:
+			std::cout << "ARP";
+			break;
+		case ETHERTYPE_REVARP:
+			std::cout << "Reverse ARP";
+			break;
+		default:
+			std::cout << "Other";
+	}
+
+	std::cout << "), size: " << header->caplen << "/" << header->len << std::endl;
 }
--- src/sniffer.h
+++ src/sniffer.h
@@ -10,7 +10,7 @@ private:
 	Config *config;
 
 	static void print_interfaces();
-	static void packet_callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+	static void packet_callback(u_char *args, const struct pcap_pkthdr *header, const u_char *payload);
 
 public:
 	Sniffer(int argc, char *argv[]);
